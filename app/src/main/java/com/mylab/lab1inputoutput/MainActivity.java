14dde7ce657584ce62fb09ae2bf8a14443fffd01
Student14 <student24@iMac14s-iMac.local>
2017-09-25 09:50:06 +0800

    Initial commit

---
+++ app/src/main/java/com/mylab/lab1inputoutput/MainActivity.java
@@ -0,0 +1,38 @@
+package com.mylab.lab1inputoutput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button displayButton;
+    private EditText etName;
+    private TextView tvMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        displayButton = (Button) findViewById (R.id.button);
+        etName = (EditText) findViewById (R.id.editText);
+        tvMessage = (TextView) findViewById(R.id.textView);
+
+
+    }
+
+    public void displayText (View view){
+
+        String text = etName.getText().toString();
+
+        tvMessage.setText("Welcome!" +text);
+
+        Toast toast = Toast.makeText(this,"WELCOME", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
